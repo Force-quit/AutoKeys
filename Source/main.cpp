b265bd51466f69dbe48048c40d826a5321d91509
Force-quit <miloulune1@gmail.com>
2024-01-14 20:09:41 -0500

    Match system's theme

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
+#include <QStyleFactory>
 #include "../Headers/EQAutoClicker.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
     EQAutoClicker w;
     w.show();
     return a.exec();
